11713d3afb7f50ccacf20b2424557778c53cb42e
Tim <tschlein@gmu.edu>
2014-03-30 20:30:33 -0400

    Modified check.py - more efficient parsing of *.ini file; return TRUE / FALSE

--- check.py
+++ check.py
@@ -10,29 +10,15 @@ import configparser #https://docs.python.org/2/library/configparser.html
 import argparse     #http://docs.python.org/3.4/library/argparse.html
 
 
-#Check whether the file exists.
-def check(file):
-
-    if debug >= 1:
-        print('Entering check:')
-    if debug >= 2:
-        print('\tFile passed in:' + str(file))
-
-    if path.isfile(file):
-        if debug >= 2:
-            print('\t\t+ ', file)
-        return True
-    else:
-        print('\t\t-', file)
-        return False
-
-
-#Parse the ini file.
+#Parse the ini file to check whether dependencies are present.
 def parse(file):
     if debug >= 1:
         print('Entering parse:')
     if debug >= 2:
         print('\tConfig file passed in:' + str(file))
+
+    #Declare list of missing executables and/or modules.
+    missing = []
 
     #Determine system: 'nt' for Windows, 'posix' for *nix, Mac OSX
     system = platform.platform()
@@ -47,20 +33,30 @@ def parse(file):
     config.read(file)
 
     if 'Windows' in system:
-        #Bulk Extractor
-        if architecture == 64:
-            check(config.get('Windows', '64_bulk'))
-        else:
-            check(config.get('Windows', '32_bulk'))
-        #TShark
-        check(config.get('Windows', 'tShark'))
+        for key in config['Windows']:
+            value = config.get('Windows', key)
+            if path.isfile(value):
+                if debug >= 2:
+                    print('\t| [+]  ', value, '|')
+            else:
+                print('\t| [-]  ', value, '|')
+                missing.append(value)
 
     elif 'Linux' in system:
-        #Bulk Extractor
-        check(config.get('Linux', '32_bulk'))
-        #TShark
-        check(config.get('Linux', 'tShark'))
+        for key in config['Linux']:
+            value = config.get('Linux', key)
+            if path.isfile(value):
+                if debug >= 2:
+                    print('\t| [+]  ', value, '|')
+            else:
+                print('\t| [-]  ', value, '|')
+                missing.append(value)
 
+    #Return True if all dependencies are present; otherwise, return False.
+    if (len(missing)):
+        return False
+    else:
+        return True
 
 #Parse the command line arguments.
 def main(argv):
@@ -79,14 +75,13 @@ def main(argv):
         args = parser.parse_args()
         if args.file:
             file = args.file
-            #with codecs.open(file, 'r', 'ascii'):
-                #check = True
         if args.debug:
             debug = args.debug
         if debug >= 1:
             print('Entering Main:')
 
-        parse(file)
+        value = parse(file)
+        return(value)
 
     except IOError:
         sys.exit('Error: File ' + str(file) + ' does not exist.')
