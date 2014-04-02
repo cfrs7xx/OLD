76957b28216ddfc5384bdefb4af6f840db7546c8
Tim <tschlein@gmu.edu>
2014-04-01 20:04:16 -0400

    Changed to allow for calls from main.py:
    hashing.hashFile(file, verbose) returns boolean and error string if boolean == False
    
    hashing.hashRecursive(path, verbose) returns boolean and error string if boolean == False

--- hashing.py
+++ hashing.py
@@ -19,16 +19,14 @@ __author__ = 'tschlein'
 #
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-import os					#path, os.walk() for recursive walking of directories
-import fnmatch				#used for file matching
-#from subprocess import call	#command line invocations
+import os					    #path, os.walk() for recursive walking of directories
+import fnmatch				    #used for file matching
 import sys
 import hashlib
-import argparse             #http://docs.python.org/3.4/library/argparse.html
+import argparse                 #http://docs.python.org/3.4/library/argparse.html
 #from writer import htmlwrite
 
 def md5(file):
-
     #define blocksize for reading file
     block = 65536
     #open the file
@@ -41,12 +39,10 @@ def md5(file):
     while len(buf) > 0:
         hash.update(buf)
         buf = f.read(block)
-
     return hash
 
 
 def sha1(file):
-
     #define blocksize for reading file
     block = 65536
     #open the file
@@ -59,17 +55,84 @@ def sha1(file):
     while len(buf) > 0:
         hash.update(buf)
         buf = f.read(block)
-
     return hash
+
+
+#Call the hashing functions
+def caller(files, verbose):
+    if verbose >= 1:
+        print('[+] Entering hashing.caller: ')
+
+    #html_file = 'output.htm'
+    #htmlwrite('start','null', html_file, 'null')
+    #calculate hashes for each file within the file list
+    if verbose >= 2:
+        print(files)
+
+    for file in files:
+        md5_val = md5(file)
+        sha1_val = sha1(file)
+
+        #if file has hash value, print to screen and to html file
+        if md5_val and verbose >= 2:
+            #print formatted output string: <filename> TAB <md5 hash> TAB <sha1 hash>
+            output = file
+            output += '\t'
+            output += md5_val.hexdigest()
+            output +='\t'
+            output += sha1_val.hexdigest()
+            print(output)
+
+            #Call HTML_Writing.py from here, sending the string
+            #htmlwrite('data',output, html_file, 'null')
+
+    #htmlwrite('stop', 'null', html_file, 'null')
+
+
+#Hash a single file
+def hashFile(file, verbose):
+    if verbose >= 1:
+        print('[+] Entering hashing.hashFile: ')
+
+    if os.path.isfile(file):
+        #Declare list of files.
+        files = []
+        #add the file to the list
+        files.append(file)
+        caller(files, verbose)
+        return True
+
+    else:
+        error = '[-] File: ' + file + ' not found.'
+        return False, error
+
+
+#Recursive hash
+def hashRecursive(path, verbose):
+    if verbose >= 1:
+        print('[+] Entering hashing.hashRecursive: ')
+
+    #Declare list of files.
+    files = []
+
+    try:
+        for root, dirnames, filenames in os.walk(path):
+        #recursively walk the current path, from the top down (current directory as root)
+            for filename in fnmatch.filter(filenames, '*.*'):		#can filter by file type, ex. *.apk
+                #add the files in the present directory to the list
+                files.append(os.path.join(root, filename))
+        caller(files, verbose)
+        return True
+
+    except IOError:
+        error = '[-]: Path ' + str(path) + ' does not exist.'
+        return False, error
 
 
 def main(argv):
     try:
-        global debug
-        debug = 0
-
-        #Declare list of files.
-        files = []
+        global verbose
+        verbose = 0
 
         parser = argparse.ArgumentParser(description="Hash file(s), with recursive hashing as an option.",
                                          add_help=True)
@@ -79,50 +142,20 @@ def main(argv):
         parser.add_argument('--version', action='version', version='%(prog)s 0.5')
 
         args = parser.parse_args()
+        if args.verbose:
+            verbose = args.verbose
         if args.file:
             file = args.file
-            #add the file to the list
-            files.append(file)
+            hashFile(file, verbose)
         if args.recursive:
             path = args.recursive
-            for root, dirnames, filenames in os.walk(path):
-            #recursively walk the current path, from the top down (current directory as root)
-                for filename in fnmatch.filter(filenames, '*.*'):		#can filter by file type, ex. *.apk
-                    #add the files in the present directory to the list
-                    files.append(os.path.join(root, filename))
-        if args.verbose:
-            debug = args.verbose
-        if debug >= 1:
+            hashRecursive(path, verbose)
+
+        if verbose >= 1:
             print('Entering Main:')
-
-        #html_file = 'output.htm'
-        #htmlwrite('start','null', html_file, 'null')
-        #calculate hashes for each file within the file list
-        print(files)
-
-        for file in files:
-            md5_val = md5(file)
-            sha1_val = sha1(file)
-
-            #if file has hash value, print to screen and to html file
-            if md5_val:
-                #print formatted output string: <filename> TAB <md5 hash> TAB <sha1 hash>
-                output = file
-                output += '\t'
-                output += md5_val.hexdigest()
-                output +='\t'
-                output += sha1_val.hexdigest()
-                print(output)
-                #In the future, may return(output) to a driver program, and that program forwards string to HTML_Writing
-
-                #Call HTML_Writing.py from here, sending the string
-                #htmlwrite('data',output, html_file, 'null')
-
-        #htmlwrite('stop', 'null', html_file, 'null')
 
     except IOError:
         sys.exit('Error: File ' + str(file) + ' does not exist.')
-
 
 #Call main
 main(sys.argv[1:])
